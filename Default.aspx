9301decc855f9e2207f58a7c7bffa8b6e78385af
Rituparno Ghosh <60483448+RituCs@users.noreply.github.com>
2024-12-02 12:45:07 +0530

    First Upload

---
+++ Default.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="crud_first_version._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <table class="table-style">
+        <tr>
+            <td colspan="2" class="header">CRUD OPERATION</td>
+        </tr>
+        <tr>
+            <td>Product Id</td>
+            <td>
+                <asp:TextBox runat="server" ID="productid"></asp:TextBox><asp:RequiredFieldValidator runat="server" ErrorMessage="Enter Product Id" ForeColor="Red" ControlToValidate="productid"></asp:RequiredFieldValidator></td>
+        </tr>
+        <tr>
+            <td>Item Name</td>
+            <td>
+                <asp:TextBox runat="server" ID="itemname"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Specification</td>
+            <td>
+                <asp:TextBox runat="server" ID="specification"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>Unit</td>
+            <td>
+                <asp:DropDownList runat="server" ID="unit">
+                 <asp:ListItem Value="" >Select</asp:ListItem>
+                <asp:ListItem>PCS</asp:ListItem>
+                <asp:ListItem>KG</asp:ListItem>
+                <asp:ListItem>Ltr</asp:ListItem>
+                <asp:ListItem>DZ</asp:ListItem>
+            </asp:DropDownList></td>
+        </tr>
+        <tr>
+            <td>Status</td>
+            <td>
+                <asp:RadioButtonList runat="server" RepeatDirection="Horizontal" ID="status">
+                    <asp:ListItem>Running</asp:ListItem>
+                    <asp:ListItem>Unused</asp:ListItem>
+                </asp:RadioButtonList></td>
+        </tr>
+        <tr>
+            <td>Creation Date</td>
+            <td>
+                <asp:TextBox runat="server" ID="creationdate" TextMode="Date"></asp:TextBox></td>
+        </tr>
+         <tr>
+          <td></td>
+
+          <td>
+         <asp:Button ID="Button1" runat="server" Text="Insert" BackColor="#7494ec" ForeColor="	#FFFFFF" OnClick="Button1_Click" />
+              <asp:Button runat="server" Text="Update" BackColor="#7494ec" ForeColor="#FFFFFF"  OnClick="Unnamed1_Click"></asp:Button>
+              <asp:Button runat="server" Text="Delete" BackColor="#7494ec" ForeColor="	#FFFFFF" OnClientClick="return confirm('are you sure to Delete');" OnClick="Unnamed2_Click"></asp:Button>
+          </td>
+
+            
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:GridView ID="GridView1" runat="server"  CssClass="gridview-style" HeaderStyle-BackColor="#7494ec" HeaderStyle-ForeColor="#FFFFFF"></asp:GridView>
+            </td>
+        </tr>
+    </table>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.UI;
+using System.Configuration;
+
+namespace crud_first_version
+{
+    public partial class _Default : Page
+    {
+        //string connectionString = "Data Source=DESKTOP-ER2ICMK\\SQLEXPRESS;Initial Catalog=Crud_DB;Integrated Security=True;Encrypt=False";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+
+            {
+                unit.SelectedIndex = 0;
+                GetProductList();
+                ClearForm();
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FieldValidation())
+            {
+                try
+                {
+                    using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cruddb"].ConnectionString))
+                    {
+                        con.Open();
+
+                        // Get input values
+                        int productID = int.Parse(productid.Text);
+                        string iname = itemname.Text;
+                        string spec = specification.Text;
+                        string unt = unit.SelectedValue;
+                        string sta = status.SelectedValue;
+                        DateTime cdate = DateTime.Parse(creationdate.Text);
+
+                        // Use parameterized query to prevent SQL injection
+                        using (SqlCommand cmd = new SqlCommand("exec ProductSetup_SP @ProductID, @ItemName, @Specification, @Unit, @Status, @CreationDate", con))
+                        {
+                            cmd.Parameters.AddWithValue("@ProductID", productID);
+                            cmd.Parameters.AddWithValue("@ItemName", iname);
+                            cmd.Parameters.AddWithValue("@Specification", spec);
+                            cmd.Parameters.AddWithValue("@Unit", unt);
+                            cmd.Parameters.AddWithValue("@Status", sta);
+                            cmd.Parameters.AddWithValue("@CreationDate", cdate);
+
+                            // Execute the command
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        // Success message
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully inserted');", true);
+                    }
+
+                    // Refresh the product list
+                    GetProductList();
+                    ClearForm();
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", $"alert('Error: {ex.Message}');", true);
+
+                }
+            }
+        }
+
+        void GetProductList()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cruddb"].ConnectionString))
+                {
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("exec ProductList_SP", con))
+                    {
+                        SqlDataAdapter sd = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        sd.Fill(dt);
+
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cruddb"].ConnectionString))
+                {
+                    con.Open();
+                    int result;
+                    // Get input values
+                    int productID = int.Parse(productid.Text);
+                    string iname = itemname.Text;
+                    string spec = specification.Text;
+                    string unt = unit.SelectedValue;
+                    string sta = status.SelectedValue;
+                    DateTime cdate = DateTime.Parse(creationdate.Text);
+
+                    // Use parameterized query to prevent SQL injection
+                    using (SqlCommand cmd = new SqlCommand("exec ProductUpdate_SP @ProductID, @ItemName, @Specification, @Unit, @Status, @CreationDate", con))
+                    {
+                        cmd.Parameters.AddWithValue("@ProductID", productID);
+                        cmd.Parameters.AddWithValue("@ItemName", iname);
+                        cmd.Parameters.AddWithValue("@Specification", spec);
+                        cmd.Parameters.AddWithValue("@Unit", unt);
+                        cmd.Parameters.AddWithValue("@Status", sta);
+                        cmd.Parameters.AddWithValue("@CreationDate", cdate);
+
+                        // Execute the command
+                        result = cmd.ExecuteNonQuery();
+                    }
+
+                    // Success message
+                    if (result > 0)
+                    {
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+
+                    }
+                    else
+                    {
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Prduct ID Not found');", true);
+
+                    }
+                }
+
+                // Refresh the product list
+                GetProductList();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", $"alert('Error: {ex.Message}');", true);
+
+            }
+        }
+
+        protected void Unnamed2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cruddb"].ConnectionString))
+                {
+                    con.Open();
+
+                    // Get input values
+                    int productID = int.Parse(productid.Text);
+
+                    // Use parameterized query to prevent SQL injection
+                    using (SqlCommand cmd = new SqlCommand("exec ProductDelete_SP @ProductID", con))
+                    {
+                        cmd.Parameters.AddWithValue("@ProductID", productID);
+
+                        // Execute the command
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Success message
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+
+                }
+
+                // Refresh the product list
+                GetProductList();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", $"alert('Error: {ex.Message}');", true);
+            }
+        }
+    
+        bool FieldValidation()
+        {
+            if (string.IsNullOrWhiteSpace(productid.Text) ||
+                string.IsNullOrWhiteSpace(itemname.Text)|| 
+                string.IsNullOrWhiteSpace(specification.Text) ||
+               string.IsNullOrWhiteSpace(creationdate.Text) ||
+               string.IsNullOrEmpty(status.SelectedValue) ||
+               string.IsNullOrEmpty(unit.SelectedValue)
+               )
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Please fill all required field');", true);
+                return false;
+            }
+            return true;
+        }
+        void ClearForm()
+        {
+            productid.Text = itemname.Text = specification.Text = creationdate.Text = "";
+            unit.ClearSelection();
+            unit.SelectedIndex = 0;
+            status.ClearSelection();
+        }
+    
+    
+    
+    }
+}
